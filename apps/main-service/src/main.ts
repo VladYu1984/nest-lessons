c5df9d3a334aa2d3472a275af65af60068f50063
discoBoychik <v.bobylev@holiverse.ai>
2025-12-26 16:39:09 +0400

    Patch 1.0.4.1 Fix share static for frontend

--- apps/main-service/src/main.ts
+++ apps/main-service/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     await NestFactory.create<NestExpressApplication>(MainServiceModule);
   app.use(cookieParser());
 
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
     prefix: '/uploads',
   });
 
